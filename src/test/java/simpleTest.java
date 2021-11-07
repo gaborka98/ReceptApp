75f79d848553b9d16d067e29ac8b909f0db956c2
Horváth Gábor <gaborka812@gmail.com>
2021-11-07 13:01:20 +0100

    Add test for maven

---
+++ src/test/java/simpleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class simpleTest {
+    @Test
+    void firstTest() {
+        Integer a = null;
+        Assertions.assertNull(a);
+    }
+}
